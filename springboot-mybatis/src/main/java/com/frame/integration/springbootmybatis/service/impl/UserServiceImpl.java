366985c43bc3a4360fe8c41368d9c4a624101256
wisdom-jiang <jdz199409@163.com>
2018-08-15 17:56:30 +0800

    Create UserServiceImpl.java

---
+++ springboot-mybatis/src/main/java/com/frame/integration/springbootmybatis/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.frame.integration.springbootmybatis.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.frame.integration.springbootmybatis.service.UserService;
+
+@Service(value="userService")
+public class UserServiceImpl implements UserService {
+
+}
