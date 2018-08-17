1db6087b148452ddc8d828d632c1895cbf12e07c
wisdom-jiang <jdz199409@163.com>
2018-08-17 17:50:37 +0800

    Update UserService.java

--- springboot-mybatis/src/main/java/com/frame/integration/springbootmybatis/service/UserService.java
+++ springboot-mybatis/src/main/java/com/frame/integration/springbootmybatis/service/UserService.java
@@ -1,5 +1,20 @@
 package com.frame.integration.springbootmybatis.service;
 
-public interface UserService {
+import java.util.List;
 
+import com.frame.integration.springbootmybatis.entity.User;
+
+public interface UserService {
+    // 增 insert
+    int insertUser(User user);
+    
+    // 删 delete
+    int deleteByPrimarykey(int user_id);
+    
+    // 改 update
+    int updateUserByPrimarykey(User user);
+    
+    // 查 select
+    User selectUserByPrimarykey(int user_id);
+    List<User> selectAllUserList();
 }
