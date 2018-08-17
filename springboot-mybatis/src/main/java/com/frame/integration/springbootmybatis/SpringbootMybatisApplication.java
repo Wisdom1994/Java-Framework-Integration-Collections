edb94bbbaf7c940ba05865c438c7d9d56df55d86
wisdom-jiang <jdz199409@163.com>
2018-08-17 17:51:46 +0800

    Update SpringbootMybatisApplication.java

--- springboot-mybatis/src/main/java/com/frame/integration/springbootmybatis/SpringbootMybatisApplication.java
+++ springboot-mybatis/src/main/java/com/frame/integration/springbootmybatis/SpringbootMybatisApplication.java
@@ -1,9 +1,11 @@
 package com.frame.integration.springbootmybatis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.frame.integration.springbootmybatis.dao")
 public class SpringbootMybatisApplication {
 
 	public static void main(String[] args) {
