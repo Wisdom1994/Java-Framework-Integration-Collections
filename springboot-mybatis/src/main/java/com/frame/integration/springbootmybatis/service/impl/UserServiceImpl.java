b3c8012ac5b6335c086f592e04859de78fe618f8
wisdom-jiang <jdz199409@163.com>
2018-08-17 17:50:12 +0800

    Update UserServiceImpl.java

--- springboot-mybatis/src/main/java/com/frame/integration/springbootmybatis/service/impl/UserServiceImpl.java
+++ springboot-mybatis/src/main/java/com/frame/integration/springbootmybatis/service/impl/UserServiceImpl.java
@@ -1,10 +1,43 @@
 package com.frame.integration.springbootmybatis.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.frame.integration.springbootmybatis.dao.UserDao;
+import com.frame.integration.springbootmybatis.entity.User;
 import com.frame.integration.springbootmybatis.service.UserService;
 
 @Service(value="userService")
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+    
+    @Override
+    public int insertUser(User user) {
+        return userDao.insertUser(user);
+    }
+
+    @Override
+    public int deleteByPrimarykey(int user_id) {
+        return userDao.deleteByPrimarykey(user_id);
+    }
+
+    @Override
+    public int updateUserByPrimarykey(User user) {
+        return userDao.updateUserByPrimarykey(user);
+    }
+
+    @Override
+    public User selectUserByPrimarykey(int user_id) {
+        return userDao.selectUserByPrimarykey(user_id);
+    }
+
+    @Override
+    public List<User> selectAllUserList() {
+        return userDao.selectAllUserList();
+    }
 
 }
